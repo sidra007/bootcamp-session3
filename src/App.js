dded5f96b0f277e151317d5e98b629705bd57001
sidra007 <engr.sidra@hotmail.com>
2020-09-26 10:32:33 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Dinner from './Dinner.js'
+
+function App (){
+  return(
+    <Dinner dishname="burger"/>
+  );
+}
+export default App;
---
+++ src/Dinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Dinner(props){
+    return(
+    <h1> today we r serving {props.dishname}</h1>
+    );
+}
+export default Dinner;
